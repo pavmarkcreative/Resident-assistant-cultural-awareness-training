1c78f2ccb61edd5c0b3f4721f3dabebfde605139
pavmarkcreative <pavmarkcreative@gmail.com>
2026-07-02 11:09:22 -0700

    Initial upload of Resident Assistant Cultural Awareness Training
    Initial upload of Resident Assistant Cultural Awareness Training

---
+++ story_content/triggers.js
@@ -0,0 +1,98 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6CKcIBjzcKg":
+        Script1();
+        break;
+      case "5riTy1qelmq":
+        Script2();
+        break;
+      case "6rbCTQNtNSu":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6Q53ygIUHHD');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5g7ntfnzS2I';
+const teeterAmount = 4;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6o6zxPdegK5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5vmYiNXu6lV';
+const teeterAmount = 4;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6AaUqwocBRB');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5VaN30jj7Pb';
+const teeterAmount = 4;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
